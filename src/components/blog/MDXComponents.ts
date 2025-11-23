826bbb0f51737b76442f0e532c1eeb8494f45e19
TanukiMCP <tanukimcp@gmail.com>
2025-11-22 18:02:42 -0700

    Add TripCalculator and HotelComparison to MDXComponents for consistency
    - Export TripCalculator and HotelComparison in MDXComponents.ts
    - Add EventPlanningTimeline export (was missing)
    - Organize exports by category (blog components, then planning components)
    - Ensures all components available in BlogLayout are also exported in MDXComponents

--- src/components/blog/MDXComponents.ts
+++ src/components/blog/MDXComponents.ts
@@ -3,10 +3,13 @@ export { ArticleBookingWidget } from './ArticleBookingWidget';
 export { HotelShowcase } from './HotelShowcase';
 export { HotelGrid } from './HotelGrid';
 export { ActivityShowcase } from './ActivityShowcase';
+export { TripCalculator } from '@/components/planning/TripCalculator';
+export { HotelComparison } from '@/components/planning/HotelComparison';
+export { SeasonComparison } from '@/components/planning/SeasonComparison';
+export { GroupTypeRecommender } from '@/components/planning/GroupTypeRecommender';
+export { EventPlanningTimeline } from '@/components/planning/EventPlanningTimeline';
 export { GroupCostCalculator } from '@/components/planning/GroupCostCalculator';
 export { HotelSplitCalculator } from '@/components/planning/HotelSplitCalculator';
 export { CostPerPersonRanking } from '@/components/planning/CostPerPersonRanking';
 export { BudgetToItineraryPlanner } from '@/components/planning/BudgetToItineraryPlanner';
-export { SeasonComparison } from '@/components/planning/SeasonComparison';
-export { GroupTypeRecommender } from '@/components/planning/GroupTypeRecommender';
 
